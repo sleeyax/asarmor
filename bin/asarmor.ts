9d6169a874bfd5089fd21135d55ea6f895fa319f
Sleeyax <yourd3veloper@gmail.com>
2021-11-01 19:16:54 +0100

    Update CLI

--- bin/asarmor.ts
+++ bin/asarmor.ts
@@ -1,8 +1,6 @@
-/* #!/usr/bin/env node
+#!/usr/bin/env node
 import { Command } from 'commander';
-import { Asarmor, FileCrash } from '../src';
-import Trashify from '../src/protections/trashify';
-import Bloat from '../src/protections/bloat';
+import {open, createBloatPatch, createTrashPatch} from '../src';
 const { version } = require('../package.json');
 
 const program = new Command();
@@ -11,17 +9,13 @@ program
 	.version(version)
 	.option('-a, --archive <archive>', 'input asar file (required)')
 	.option('-o, --output <output>', 'output asar file (required)')
-	.option('-v, --verbose', 'enable verbose console output')
 	.option('-b, --backup', 'create backup')
-	.option('-r, --restore', 'restore backup (protections aren\'t applied)')
-	.option('-f, --filetocrash <filename size...>', 'corrupt specified file within the archive')
+	.option('-r, --restore', 'restore backup')
 	.option('-bl, --bloat [gigabytes]', 'add huge random files to disk on extraction attempt')
 	.option('-t, --trashify [junkfiles...]', 'add fake files to the archive')
 	.on('--help', () => {
 		console.log('');
 		console.log('Examples:');
-		console.log('  $ asarmor -a app.asar -o asarmor.asar --filetocrash index_dummy.js');
-		console.log('  $ asarmor -a app.asar -o asarmor.asar --filetocrash index_dummy.js -999');
 		console.log('  $ asarmor -a app.asar -o asarmor.asar --bloat 1000');
 		console.log('  $ asarmor -a app.asar -o asarmor.asar --trashify bee-movie.txt foo.js bar.ts');
 		console.log('  $ asarmor -a app.asar -o asarmor.asar --trashify --backup');
@@ -34,24 +28,25 @@ if (!program.archive || !program.output) {
 	program.exit();
 }
 
-if (program.verbose) process.env.VERBOSE = 'true';
-
-const asarmor = new Asarmor(program.archive);
-
-if (program.restore) {
-	asarmor.restoreBackup();
+async function main() {
+	const asarmor = await open(program.archive);
+	
+	if (program.restore) {
+		asarmor.restoreBackup();
+	}
+	else if (program.output) {
+		if (program.backup)
+			asarmor.createBackup();
+	
+		if (program.trashify)
+			asarmor.patch(createTrashPatch({
+				filenames: program.trashify === true ? undefined : program.trashify,
+			}));
+		if (program.bloat)
+			asarmor.patch(createBloatPatch(program.bloat === true ? undefined : program.bloat));
+	
+		await asarmor.write(program.output);
+	}
 }
-else if (program.output) {
-	if (program.backup)
-		asarmor.createBackup();
 
-	if (program.filetocrash)
-		asarmor.applyProtection(new FileCrash(program.filetocrash[0], +program.filetocrash[1]));
-	if (program.trashify)
-		asarmor.applyProtection(new Trashify(program.trashify === true ? undefined : program.trashify));
-	if (program.bloat)
-		asarmor.applyProtection(new Bloat(program.bloat === true ? undefined : program.bloat));
-
-	asarmor.write(program.output).catch(console.error);
-}
- */
+main();
