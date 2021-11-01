719a302f2d1f856215e978b5ce59eaf63e84cd50
Sleeyax <yourd3veloper@gmail.com>
2021-11-01 22:51:03 +0100

    Simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import Asarmor, {open, close} from './asarmor';
-import {Archive, File, FileEntries, Header} from './asar';
-import {Patch, createBloatPatch, createTrashPatch } from './patch';
-
-export {Asarmor, open, close, Archive, File, FileEntries, Header, Patch, createBloatPatch, createTrashPatch};
+export * from './asarmor';
+export * from './asar';
+export * from './patch';
