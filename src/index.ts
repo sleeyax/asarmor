ec34de215debd0c975fd026b838f394768e7762e
Sleeyax <yourd3veloper@gmail.com>
2021-11-01 22:45:54 +0100

    Export patch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Asarmor, {open, close} from './asarmor';
 import {Archive, File, FileEntries, Header} from './asar';
-import {createBloatPatch, createTrashPatch} from './patch';
+import {Patch, createBloatPatch, createTrashPatch } from './patch';
 
-export {Asarmor, open, close, Archive, File, FileEntries, Header, createBloatPatch, createTrashPatch};
+export {Asarmor, open, close, Archive, File, FileEntries, Header, Patch, createBloatPatch, createTrashPatch};
